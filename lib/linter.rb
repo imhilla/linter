efc492f654f33e207e7bff2bd8f1289c8492efdb
imhilla <hillaryodhiambo282@gmail.com>
2020-06-11 10:53:10 +0300

    add linter class and include other classes

---
+++ lib/linter.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/space_check.rb'
+require_relative '../lib/syntax_check.rb'
+
+class Linter
+    include 
+    include Scanner
+  end
--- lib/scanner.rb
+++ lib/scanner.rb
@@ -1,1 +1,10 @@
-# lib/scanner.rb
+#lib/scanner.rb
+module Scanner
+  def missing_semicolon(lines, errors)
+    lines.each_with_index do |line, index|
+      next if line.include?('{') || line.include?('}') || line == "/n" || line.end_with?(",/n") 
+      errors << "Line doesnot end with ';' on line #{index + 1}." unless line.end_with?(";/n") 
+    end
+    errors
+  end
+end
