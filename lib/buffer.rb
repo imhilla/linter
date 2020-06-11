e09bb45ac3b6c8626c91326d9133e55538ca7cdc
imhilla <hillaryodhiambo282@gmail.com>
2020-06-11 18:56:58 +0300

    add buffer to open and check file

--- lib/buffer.rb
+++ lib/buffer.rb
@@ -1,1 +1,16 @@
-require 'linter.rb'
+require '../lib/linter.rb'
+module Buffer
+  def self.check(file) 
+    errors = []
+    lines = File.open(file).to_a
+    if lines.size.positive?
+      check = Linter.new
+      check.last_line(lines, errors)
+      check.indentation(lines, errors)
+      check.trailing_space(lines, errors)
+      check.missing_semicolon(lines, errors)
+      check.space_before_bracket(lines, errors)
+    end
+    errors
+  end
+end
