d197c1eb4287ce3e21d6ca46e7c3c9c1438633af
imhilla <hillaryodhiambo282@gmail.com>
2020-06-11 11:53:50 +0300

    add def for trailing space

--- lib/space_scanner.rb
+++ lib/space_scanner.rb
@@ -1,3 +1,8 @@
- module SpaceScanner 
- # rubocop:disable Metrics/CyclomaticComplexity
- end
+module SpaceScanner
+  def trailing_space(lines, errors)
+    lines.each_with_index do |line, index|
+      errors << "Trailing space found on line #{index + 1}." if line.end_with?('; /n')
+    end
+    errors
+  end
+end
