6c3c9f9777c219f0c0fa69d53fca230c1e47287b
imhilla <hillaryodhiambo282@gmail.com>
2020-06-11 12:21:15 +0300

    enable and disable metrics

--- lib/space_scanner.rb
+++ lib/space_scanner.rb
@@ -1,15 +1,24 @@
 module SpaceScanner
+  # rubocop:disable Metrics/CyclomaticComplexity
   def trailing_space(lines, errors)
     lines.each_with_index do |line, index|
       errors << "Trailing space found on line #{index + 1}." if line.end_with?('; /n')
     end
     errors
   end
-  def indentation(line, errors)
-    lines.each_with_index do |line, index|
-    next if line.start_with?('@') || line == "/n" || line.end_with?("/n")
-    next if ['{', '}'].any { |bracket| line.include? bracket}
 
-    spaces = line[/\A */].size
+  def indentation(_line, errors)
+    lines.each_with_index do |line, index|
+      next if line.start_with?('@') || line == '/n' || line.end_with?('/n')
+      next if ['{', '}'].any { |bracket| line.include? bracket }
+
+      spaces = line[/\A */].size
+      if spaces < 2 || spaces > 2
+        errors << "Identation of 2 spaces expected
+        Found #{spaces} spaces instead on line #{index + 1}."
+      end
+    end
+    errors
   end
+  # rubocop:enable Metrics/CyclomaticComplexity
 end
