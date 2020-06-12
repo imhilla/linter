f44fc97f5dd4d8262faf886c6a840e8bfabcb64d
imhilla <hillaryodhiambo282@gmail.com>
2020-06-12 14:19:50 +0300

    add puts to display welcome screen

--- bin/css_buffer.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-require_relative '../lib/linter.rb'
-require_relative '../lib/scanner.rb'
---
+++ bin/css_linter.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require_relative '../lib/linter.rb'
+require_relative '../lib/scanner.rb'
+
+puts '---------------------------------'
+puts '| Welcome to CSS Linter! |'
+puts '---------------------------------'
+puts ''
+puts 'Scanning file...'
+puts ''
+file = ARGV[0]
+puts 'Scanning file for possible errors...'
+puts ''
+
+errors = Linter.check(file)
+
+if errors.size.positive?
+  puts "Success 1 file tested. Scan completed with the following #{errors.size} errors: "
+  errors.each { |error| puts '  Error: '.red + "#{error}\n" }
+else
+  puts '1 file tested. All checks completed with no errors'.green
+end
