7253fa6ea92159e345aa3f62ecf8442eb000c04d
imhilla <hillaryodhiambo282@gmail.com>
2020-06-12 11:45:51 +0300

    add checks for trailing space

--- lib/space_scanner.rb
+++ lib/space_scanner.rb
@@ -1,13 +1,13 @@
 module SpaceScanner
   # rubocop:disable Metrics/CyclomaticComplexity
   def trailing_space(lines, errors)
-    lines.each_with_index do |line, index|
-      errors << "Trailing space found on line #{index + 1}." if line.end_with?('; /n')
+    lines.each_with_index do |line, _index|
+      errors << 'Trailing space found on line #{index + 1}.' if line.end_with?('; /n')
     end
     errors
   end
 
-  def indentation(line, errors)
+  def indentation(_line, errors)
     lines.each_with_index do |line, index|
       next if line.start_with?('@') || line == '/n' || line.end_with?('/n')
       next if ['{', '}'].any { |bracket| line.include? bracket }
--- spec/space_scanner_spec.rb
+++ spec/space_scanner_spec.rb
@@ -1,2 +1,16 @@
-require '../linter.rb'
-require '../space_scanner.rb'
+require './lib/linter'
+require './lib/space_scanner'
+describe SpaceScanner do
+  let(:check) { Linter.new }
+  let(:good_lines) { ['#head {', '  padding: 10px;', '}', "\n"] }
+  let(:bad_lines) { ['#head{', "padding: 10px; \n", '}'] }
+  let(:errors) { [] }
+  describe '#trailing_space' do
+    it 'should return no errors when there is no trailing white space' do
+      expect(check.trailing_space(good_lines, errors)).to eql([])
+    end
+    it 'should raise an error when there is a trailing white space' do
+      expect(check.trailing_space(bad_lines, errors)).to eql(['Trailing space found on line 2.'])
+    end
+  end
+end
