3fb40dab0ee2e400c28fadcedfa8c7940c604d4b
imhilla <hillaryodhiambo282@gmail.com>
2020-06-15 11:02:50 +0300

    add dummy lets

--- spec/buffer_spec.rb
+++ spec/buffer_spec.rb
@@ -3,6 +3,7 @@ require_relative '../lib/buffer'
 
 describe Buffer do
   let(:check) { Linter.new }
+  let(:lines) { File.open(file).to_a }
   let(:errors) { [] }
   describe '#self.check(file)' do
     it ''do
